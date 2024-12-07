27808af3a3736e98359332fa8263795d5c856a82
jalepeno noodle <jalepenonoodle@gmail.com>
2024-12-06 16:34:58 -0800

    video9 Complete: Added pinecones.

--- src/main/java/net/jalepenonoodle/tutorialmod/item/ModItems.java
+++ src/main/java/net/jalepenonoodle/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jalepenonoodle.tutorialmod.item;
 
 import net.jalepenonoodle.tutorialmod.TutorialMod;
+import net.jalepenonoodle.tutorialmod.item.custom.FuelItem;
 import net.jalepenonoodle.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +23,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry", 
         () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+    
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone", 
+        () -> new FuelItem(new Item.Properties(), 400));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
